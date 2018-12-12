e9c51c8a3fd31e92cc3ae8a6d478d15de3444b91
tmrfouad <tmrfouad@gmail.com>
2018-12-12 13:38:13 +0200

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+  'mongodb://todos-app-api-user:3tbkEEq3JFpTbQIq@cluster0-shard-00-00-9abmb.mongodb.net:27017,cluster0-shard-00-01-9abmb.mongodb.net:27017,cluster0-shard-00-02-9abmb.mongodb.net:27017/TodoApp?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+);
 
 module.exports = { mongoose };
